8eb9931281a2f35f722de26584fe8ee6786f1b89
yaohui <923447324@qq.com>
2019-11-10 19:43:46 +0800

    right control

---
+++ src/views/error-page/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h5>404页面</h5>
+    <p>内容被偷走了～ 。～</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {}
+    },
+    watch: {},
+    computed: {},
+    methods: {},
+    created() {},
+    mounted() {}
+  }
+</script>
---
+++ src/views/permission/page-user.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="pageUser">
+    <el-card>
+      <div class="driver_p0">
+        <i class="el-icon-s-opportunity"></i>你的权限页面是:
+      </div>
+      <el-tag>没做呀  愁</el-tag>
+    </el-card>
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+  }
+</script>
+<style scoped lang="scss">
+  .pageUser .el-tag {
+    margin-right: 10px;
+    margin-bottom: 10px;
+  }
+  .driver_p0 {
+    font-size: 14px;
+    margin-bottom: 20px;
+    i {
+      margin-right: 5px;
+      color: #ffc107;
+      font-size: 18px;
+    }
+  }
+</style>
